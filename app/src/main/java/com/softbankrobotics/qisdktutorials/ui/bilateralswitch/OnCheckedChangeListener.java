aa7188d068850b37b10d81323bcc34936c4dca72
Leonards <leonards.elksnis@gmail.com>
2021-08-26 12:08:29 +0300

    Initial push for qiSDK tutorial github project

---
+++ app/src/main/java/com/softbankrobotics/qisdktutorials/ui/bilateralswitch/OnCheckedChangeListener.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 Softbank Robotics Europe
+ * See COPYING for the license
+ */
+
+package com.softbankrobotics.qisdktutorials.ui.bilateralswitch;
+
+
+public interface OnCheckedChangeListener {
+    void onCheckedChanged(boolean isChecked);
+}
